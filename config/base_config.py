bf255466df56f086f7e1078ad896656232661c69
Xiang.CC <fffhzxsxcc@163.com>
2018-06-05 09:39:03 +0800

    Update base_config.py

--- config/base_config.py
+++ config/base_config.py
@@ -3,6 +3,7 @@ import os.path as osp
 import numpy as np
 # `pip install easydict` if you don't have it
 from easydict import EasyDict as edict
+import yaml
 
 __C = edict()
 # Consumers can get config by:
@@ -199,7 +200,6 @@ def merge_a_into_b(a, b):
 
 def cfg_from_file(filename):
     """Load a config file and merge it into the default options."""
-    import yaml
     with open(filename, 'r') as f:
         yaml_cfg = edict(yaml.load(f))
 
